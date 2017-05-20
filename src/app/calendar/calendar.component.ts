2f2300704ed906ee9a2cf3041365e516f916a1df
Вадим Стецюк < >
2017-05-20 11:57:09 +0300

    Created calendar grid

---
+++ src/app/calendar/calendar.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit } from '@angular/core';
+import { MdDialog, MdDialogRef } from '@angular/material';
+import { Calendar } from '../models/Calendar';
+
+@Component({
+  selector: 'calendar',
+  templateUrl: './calendar.component.html',
+  styleUrls: ['./calendar.component.css']
+})
+export class CalendarComponent implements OnInit {
+  calendar: Calendar;
+  selectedDate: any;
+
+  constructor() {
+    this.calendar = new Calendar();
+  }
+
+  ngOnInit() {
+    let date = new Date();
+    this.calendar.currDate = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+
+    this.calendar.week = [{ title: 'Monday', color: 'lightgrey' },
+    { title: 'Tuesday', color: 'lightgrey' },
+    { title: 'Wednesday', color: 'lightgrey' },
+    { title: 'Thursday', color: 'lightgrey' },
+    { title: 'Friday', color: 'lightgrey' },
+    { title: 'Saturday', color: 'lightgrey' },
+    { title: 'Sunday', color: 'lightgrey' }];
+
+    this.createCalendarFromDate(this.calendar.currDate);
+  }
+
+  public createCalendarFromDate(date: Date) {
+    console.log(this.calendar.currDate.getFullYear() + " " + (this.calendar.currDate.getMonth() + 1) + " " + this.calendar.currDate.getDate());
+
+    var firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+    let lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+
+    /* 
+      Fill the array of days for current month
+    */
+    for (let i = 1, k = 0; i <= lastDay.getDate(); k++) {
+      if (k < firstDay.getDay() - 1) {
+        this.calendar.days.push('');
+      } else {
+        this.calendar.days.push(i.toString());
+        i++;
+      }
+    }
+  }
+
+  incrementMonth() {
+    this.calendar.days = [];
+
+    if (this.calendar.currDate.getMonth() === 11) {
+      this.calendar.currDate = new Date(this.calendar.currDate.getFullYear() + 1, 0,  // increment year and month
+                                this.calendar.currDate.getDate());
+    } else {
+      this.calendar.currDate = new Date(this.calendar.currDate.getFullYear(), 
+                                this.calendar.currDate.getMonth() + 1, this.calendar.currDate.getDate());
+    }
+    this.createCalendarFromDate(this.calendar.currDate);
+  }
+
+  decrementMonth() {
+    this.calendar.days = [];
+
+    if (this.calendar.currDate.getMonth() === 0) {
+      this.calendar.currDate = new Date(this.calendar.currDate.getFullYear() - 1, 11,  // decrement year and month
+                                this.calendar.currDate.getDate());
+    } else {
+      this.calendar.currDate = new Date(this.calendar.currDate.getFullYear(), 
+                                this.calendar.currDate.getMonth() - 1, this.calendar.currDate.getDate());
+    }
+    this.createCalendarFromDate(this.calendar.currDate);
+  }
+}
