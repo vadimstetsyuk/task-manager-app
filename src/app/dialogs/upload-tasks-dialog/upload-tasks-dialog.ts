928542aa3b187057e7b62fade9c9865589291831
Вадим Стецюк < >
2017-05-22 17:39:01 +0300

    Added tab-menu below toolbar

--- src/app/dialogs/upload-tasks-dialog/upload-tasks-dialog.ts
+++ src/app/dialogs/upload-tasks-dialog/upload-tasks-dialog.ts
@@ -2,6 +2,7 @@ import { Component, Inject } from '@angular/core';
 import { MdDialogRef, MD_DIALOG_DATA, MdInputContainer } from '@angular/material';
 import { Task } from '../../models/Task';
 import { TaskService } from '../../services/task.service';
+import { LocalStorageService } from 'angular-2-local-storage';
 
 
 @Component({
@@ -12,22 +13,27 @@ export class UploadTasksDialog {
     tasks: Task[];
 
     constructor(public dialogRef: MdDialogRef<UploadTasksDialog>,
-        private taskService: TaskService) {
+        private taskService: TaskService,
+        private localStorageService: LocalStorageService) {
     }
 
     uploadTasks(url: MdInputContainer) {
         console.log(url._mdInputChild.value);
         let URL = url._mdInputChild.value;
 
+        this.getTasks(URL);
+
+        setTimeout(() => {
+            this.localStorageService.set('tasks', this.tasks);
+        }, 1000);
+    }
+
+    getTasks(URL) {
         this.taskService.getTasks(URL)
             .subscribe(
             tasks => this.tasks = tasks,
             err => {
                 console.log(err);
             });
-
-            if(this.tasks) {
-                console.log(this.tasks);
-            }
     }
 }
