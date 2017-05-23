ac3e25997689b1f625be5f920c5125f1962a73d8
Вадим Стецюк < >
2017-05-23 16:47:07 +0300

    Added time to Task model

--- src/app/models/Task.ts
+++ src/app/models/Task.ts
@@ -1,6 +1,7 @@
 export class Task {
     title: string;
     start: string;
+    time: string;
     duration: number;
     priority: number;
     description: string;
@@ -8,6 +9,7 @@ export class Task {
     constructor() {
         this.title = '';
         this.start = '';
+        this.time = '';
         this.duration = 0;
         this.priority = 1;
         this.description = '';
