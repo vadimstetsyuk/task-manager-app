06c27c1b47ba63a7c571bc1c3a38bb96a5c0c893
Вадим Стецюк < >
2017-06-07 16:48:17 +0300

    Added search panel & opportunity to change a type of view

--- src/app/tasks-list/tasks-list.component.ts
+++ src/app/tasks-list/tasks-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input} from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Task } from '../models/Task';
 
 @Component({
@@ -7,10 +7,31 @@ import { Task } from '../models/Task';
     styleUrls: ['./tasks-list.component.css']
 })
 
-export class TasksListComponent {
+export class TasksListComponent implements OnInit {
     @Input() tasks: Task[];
+    filteredTasks: Task[];
+    viewType: boolean;
 
     constructor() {
         this.tasks = [];
+        this.viewType = false;
+        this.filteredTasks = [];
+    }
+
+    ngOnInit() {
+        this.filteredTasks = this.tasks;
+    }
+ 
+    filterTasks(filter: string) {
+        this.filteredTasks = [];
+
+        this.tasks.forEach(task => {
+            if(task.title.toLocaleLowerCase().includes(filter))
+                this.filteredTasks.push(task);
+        });
+    }
+
+    toogleView() {
+        this.viewType = !this.viewType;
     }
 }
